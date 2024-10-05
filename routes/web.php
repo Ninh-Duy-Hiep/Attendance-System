a2c35adc6477e8273e08171e7721c503f604b5e2
Ninh-Duy-Hiep <bignose2862003@gmail.com>
2024-10-06 00:17:10 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin/admin_dashboard', function () {
-    return view('admin.admin_dashboard');
+Route::get('/', function () {
+    return view('welcome');
 });
